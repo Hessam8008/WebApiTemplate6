c1b1cd424199829d6d0ac9a414e1a45b47c3cf19
Hessam8008 <hessam8008@gmail.com>
2022-09-28 12:49:03 +0330

    Add DomainErrors static class for errors.

---
+++ Service/src/Core/Domain/Errors/DomainErrors.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Errors;
+
+public static class DomainErrors
+{
+    public static class Person
+    {
+        public static readonly Error BlockedNationality = new("Person.Nationality", "Nationality has been blocked.");
+    }
+}
