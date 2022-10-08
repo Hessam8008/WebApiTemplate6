a3e7c544d807a2e9da0ce4ac540a263acf136ab1
Hessam <hessam8008@yahoo.com>
2022-10-08 12:31:09 +0330

    Coding UnitOfWork.SaveChangesAsync()

--- Service/src/External/Infrastructure/UnitOfWork.cs
+++ Service/src/External/Infrastructure/UnitOfWork.cs
@@ -4,9 +4,18 @@ namespace Infrastructure
 {
     public sealed class UnitOfWork : IUnitOfWork
     {
-        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public UnitOfWork(ApplicationDbContext applicationDbContext)
         {
-            throw new NotImplementedException();
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await _applicationDbContext
+                .SaveChangesAsync(cancellationToken)
+                .ConfigureAwait(false);
         }
     }
 }
