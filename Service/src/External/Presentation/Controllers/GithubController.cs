85a64dfb14fc2f01dfd810e84ff92e0ffa38d5a4
Hessam8008 <hessam8008@gmail.com>
2023-11-18 16:27:47 +0330

    Add Github controller.

---
+++ Service/src/External/Presentation/Controllers/GithubController.cs
@@ -0,0 +1,29 @@
+﻿using Application.Github.Queries.Get;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Abstractions;
+
+namespace Presentation.Controllers;
+
+[ApiController]
+[ApiVersion("1.0")]
+[ApiVersion("2.0")]
+[Route("")]
+public class GithubController : ApiController
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="sender"></param>
+    public GithubController(ISender sender) : base(sender)
+    {
+    }
+
+    /// <summary>
+    ///     Return all cities.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetOrgAsync(CancellationToken cancellationToken) =>
+        Ok(await Sender.Send(new GetAllOrgsQuery(), cancellationToken));
+}
