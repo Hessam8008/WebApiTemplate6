0ac155da725a91ac51955d1bee980aac1d264852
Hessam8008 <hessam8008@gmail.com>
2023-11-19 17:53:16 +0330

    Extract Cache provider from Persistance project.

---
+++ Service/src/External/Cache/CacheProvider.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Abstractions;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Cache;
+
+public class CacheProvider : ICacheProvider
+{
+    private static readonly SemaphoreSlim Semaphore = new(1, 1);
+    private readonly IMemoryCache _cache;
+
+    public CacheProvider(IMemoryCache memoryCache) => _cache = memoryCache;
+
+    public async Task<T?> GetCache<T>(string key) => await GetCachedResponse<T>(key);
+
+    public async Task AddCache<T>(string key, T value)
+    {
+        try
+        {
+            await Semaphore.WaitAsync();
+            var cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(5),
+                SlidingExpiration = TimeSpan.FromMinutes(2),
+                Size = 1024
+            };
+            _cache.Set(key, value, cacheEntryOptions);
+        }
+        finally
+        {
+            Semaphore.Release();
+        }
+    }
+
+    private Task<T?> GetCachedResponse<T>(string cacheKey)
+    {
+        var isAvailable = _cache.TryGetValue(cacheKey, out T? result);
+        return Task.FromResult(isAvailable ? result : default);
+    }
+}
